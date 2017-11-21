a572adc80f294adf42a74287e99aab7eb4f8a46e
saroko-dnd <isaroko@mail.ru>
2017-11-21 08:23:15 +0200

    presentation theme changed

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () =>
+    Reveal.initialize({
+        display: "flex",
+        width: 1920,
+        height: 1080,
+        margin: 0,
+        minScale: 0.5,
+        maxScale: 2
+    })
+);
